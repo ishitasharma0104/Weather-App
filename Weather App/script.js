24bacc48211a9a0301617289ed40bc6067f186f7
Ishita <ishita.iit.sharma@gmail.com>
2026-01-30 13:11:52 +0530

    Add files via upload

---
+++ Weather App/script.js
@@ -0,0 +1,17 @@
+
+
+const inputElem = document.querySelector("input");
+const buttonElem = document.querySelector("#search");
+
+buttonElem.addEventListener("click", ()=> {
+    const location = inputElem.value.trim();
+
+    
+    fn9
+
+
+   
+fn90
+
+    inputElem.value="";
+})
